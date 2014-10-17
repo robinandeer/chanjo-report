75a9018f3cfed3b99666eec74f197e8bf8d8110e
Robin Andeer <robin.andeer@gmail.com>
2014-10-17 19:53:31 +0200

    remove unused, legacy files

--- chanjo_report/extensions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .api import ChanjoAPI
-api = ChanjoAPI()
-
-from flask.ext.assets import Environment, Bundle
-assets = Environment()
-scss = Bundle('report/style.scss', filters='pyscss', output='style.css')
-assets.register('scss_all', scss)
--- chanjo_report/report/utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-
-
-def read_lines(list_path):
-  """Read and strip lines in a file."""
-  with open(list_path, 'r', encoding='utf-8') as handle:
-    return [line.rstrip() for line in handle.readlines()]
