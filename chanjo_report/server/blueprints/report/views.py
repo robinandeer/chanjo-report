06e432cf79688fab518b6f8427d1402c14671a45
Robin Andeer <robin.andeer@gmail.com>
2015-11-04 12:57:58 +0100

    add customisation options for report

--- chanjo_report/server/blueprints/report/views.py
+++ chanjo_report/server/blueprints/report/views.py
@@ -35,22 +35,36 @@ def gene(gene_id):
 @report_bp.route('/samples')
 def group(group_id=None):
     """Generate a coverage report for a group of samples."""
-    gene_ids = request.args.getlist('gene_id')
-    level = request.args.get('level', 10)
-    panel_name = request.args.get('panel_name')
-    sample_ids = request.args.getlist('sample_id')
-    show_genes = 'show_genes' in request.args
+    gene_ids = request.args.get('gene_ids')
+    if gene_ids:
+        gene_ids = gene_ids.split(',')
+    sample_ids = request.args.get('sample_ids')
+    if sample_ids:
+        sample_ids = sample_ids.split(',')
+    try:
+        level = int(request.args.get('level'))
+    except ValueError:
+        level = 10
+    customizations = {
+        'level': level,
+        'gene_ids': gene_ids,
+        'panel_name': request.args.get('panel_name'),
+        'sample_ids': sample_ids,
+        'show_genes': 'show_genes' in request.args
+    }
 
     logger.debug('fetch samples for group %s', group_id)
-    sample_objs = api.samples(group_id=group_id, sample_ids=sample_ids)
+    sample_objs = api.samples(group_id=group_id,
+                              sample_ids=customizations['sample_ids'])
     sample_dict = {sample_obj.sample_id: sample_obj
                    for sample_obj in sample_objs}
 
     logger.debug('generate base queries for report')
     group_query = filter_samples(api.query(), group_id=group_id,
-                                 sample_ids=sample_ids)
-    if gene_ids:
-        exon_ids = [exon_obj.exon_id for exon_obj in api.gene_exons(*gene_ids)]
+                                 sample_ids=customizations['sample_ids'])
+    if customizations['sample_ids']:
+        exon_ids = [exon_obj.exon_id for exon_obj
+                    in api.gene_exons(*customizations['sample_ids'])]
         panel_query = group_query.filter(Exon.exon_id.in_(exon_ids))
     else:
         panel_query = group_query
@@ -58,26 +72,28 @@ def group(group_id=None):
     logger.debug('generate general stats (gene panel)')
     key_metrics = api.means(panel_query)
     levels = api.completeness_levels()
+    if not customizations['level'] in [item[0] for item in levels]:
+        return abort(400, ('completeness level not supported: {}'
+                           .format(customizations['level'])))
 
     logger.debug('calculate diagnostic yield for each sample')
-    tx_samples = [(sample_obj, api.diagnostic_yield(sample_obj.sample_id,
-                                                    query=panel_query,
-                                                    level=level))
+    tx_samples = [(sample_obj,
+                   api.diagnostic_yield(sample_obj.sample_id,
+                                        query=panel_query,
+                                        level=customizations['level']))
                   for sample_obj in sample_objs]
 
     return render_template(
         'report/report.html',
         samples=sample_dict,
         key_metrics=key_metrics,
-        panel_name=panel_name,
+        customizations=customizations,
         levels=levels,
-        selected_level=level,
         diagnostic_yield=tx_samples,
-        genders=api.sex_check(group_id=group_id, sample_ids=sample_ids),
-        show_genes=show_genes,
+        genders=api.sex_check(group_id=group_id,
+                              sample_ids=customizations['sample_ids']),
         created_at=datetime.date.today(),
         group_id=group_id,
-        query_args=request.args,
     )
 
 
