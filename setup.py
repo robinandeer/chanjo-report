d73b056747cce50970fef3acc1f0cdcc361098a9
Robin Andeer <robin.andeer@gmail.com>
2016-03-08 15:57:01 +0100

    update

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with codecs.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(name='chanjo-report',
       # versions should comply with PEP440
-      version='3.0.2',
+      version='3.0.3',
       description='Automatically render coverage reports from Chanjo ouput',
       long_description=LONG_DESCRIPTION,
       # what does your project relate to?
