4820f7e6acf9175cbbef4731805e35d14b7eca40
Robin Andeer <robin.andeer@gmail.com>
2017-01-02 16:11:29 +0100

    use dict syntax

--- chanjo_report/server/constants.py
+++ chanjo_report/server/constants.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-LEVELS = dict(
-    10='completeness_10',
-    15='completeness_15',
-    20='completeness_20',
-    50='completeness_50',
-    100='completeness_100',
-)
+LEVELS = {
+    10: 'completeness_10',
+    15: 'completeness_15',
+    20: 'completeness_20',
+    50: 'completeness_50',
+    100: 'completeness_100',
+}
