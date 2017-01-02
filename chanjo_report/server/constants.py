5f189075fe35a4646ad4fc320968cea24cfa3bad
Robin Andeer <robin.andeer@gmail.com>
2017-01-02 16:10:03 +0100

    change levels to dict

--- chanjo_report/server/constants.py
+++ chanjo_report/server/constants.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
-LEVELS = [(10, 'completeness_10'), (15, 'completeness_15'),
-          (20, 'completeness_20'), (50, 'completeness_50'),
-          (100, 'completeness_100')]
+LEVELS = dict(
+    10='completeness_10',
+    15='completeness_15',
+    20='completeness_20',
+    50='completeness_50',
+    100='completeness_100',
+)
