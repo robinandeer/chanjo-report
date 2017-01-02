675bc8973dd145a99c041750f3b35e56d92d4082
Robin Andeer <robin.andeer@gmail.com>
2017-01-02 16:18:28 +0100

    order keys in levels dict

--- chanjo_report/server/constants.py
+++ chanjo_report/server/constants.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
 
-LEVELS = {
-    10: 'completeness_10',
-    15: 'completeness_15',
-    20: 'completeness_20',
-    50: 'completeness_50',
-    100: 'completeness_100',
-}
+LEVELS = OrderedDict([
+    (10, 'completeness_10'),
+    (15, 'completeness_15'),
+    (20, 'completeness_20'),
+    (50, 'completeness_50'),
+    (100, 'completeness_100'),
+])
