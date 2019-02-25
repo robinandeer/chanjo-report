a6c13806d55a57a8ba6606fcdb4db3a4e328723e
northwestwitch <rasi.chiara@gmail.com>
2019-02-25 10:09:39 +0100

    testing build with python 3.6

--- setup.py
+++ setup.py
@@ -99,7 +99,6 @@ setup(name='chanjo-report',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.6'
           'Environment :: Console'
       ])
