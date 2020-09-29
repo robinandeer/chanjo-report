9292b1e9fa76cfe557834ff947793ecd8f4d83fb
northwestwitch <rasi.chiara@gmail.com>
2020-09-29 12:35:51 +0200

    nope, try to return a string, to see what the object looks like

--- chanjo_report/server/blueprints/report/views.py
+++ chanjo_report/server/blueprints/report/views.py
@@ -78,7 +78,7 @@ def json_genes():
     query = query.filter(Transcript.gene_id.in_(gene_ids))
     # And sample ID
     query = query.filter(TranscriptStat.sample_id.in_(sample_ids))
-    return jsonify({'query':query.__dict__})
+    return str(query)
 
 
 @report_bp.route('/report', methods=['GET', 'POST'])
